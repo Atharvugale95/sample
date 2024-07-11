f7189f9d52abc6266ccc596b2847f16947b56a2d
Atharv Ugale <158261519+goldenknights95@users.noreply.github.com>
2024-07-11 09:11:59 +0530

    sample result 2

---
+++ result.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+class college_database{
+	protected:
+		
+		string x[3]={"22UAI11629022004","22UAI12624012005","22UAI13413062005"};
+		
+	
+		
+		
+};
+ class person:public college_database{
+		
+	public:
+		string name;
+		string prn_pass;
+		
+		void dis()
+		{
+			cout<<"enter the prn no and password \n"<<endl;
+			cin>>prn_pass;
+			for(int i=0;i<=3;i++){
+			
+		if (prn_pass==x[i])	{
+		cout<<"**************SHOWING RESULT*****************";
+		
+						
+		}
+		else{
+					cout<<" Invalid prn no or password"<<endl;
+			
+		}
+	}
+}
+			void disp()
+			{
+				cout<<"error";
+			}
+			
+		
+};
+class result:public person{
+	public:
+		
+		char grade;
+		int total_marks;
+		float cgpa;
+		
+		
+};
+int main()
+{
+	result r1;
+	r1.dis();
+	r1.disp();
+}
